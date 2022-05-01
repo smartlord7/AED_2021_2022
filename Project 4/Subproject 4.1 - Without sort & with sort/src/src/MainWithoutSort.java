08407bdcf70a5da10eb15149256dd8cabf78fef1
Sancho Amaral Simões <ssimoes@ipn.pt>
2022-05-01 23:39:43 +0100

    MASTER: Revert changes in source code.

--- Project 4/Subproject 4.1 - Without sort & with sort/src/src/MainWithoutSort.java
+++ Project 4/Subproject 4.1 - Without sort & with sort/src/src/MainWithoutSort.java
@@ -4,154 +4,83 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class MainWithoutSort {
-    private int a;
-    private int b;
-    private static final String CMD_END = "TCHAU";
-    private static final String CMD_RASTER = "RASTER";
-    private static final String CMD_AMPLITUDE = "AMPLITUDE";
-    private static final String CMD_PERCENTILE = "PERCENTIL";
-    private static final String CMD_MEDIAN = "MEDIAN";
-    private static final String TXT_RASTER = "RASTER GUARDADO";
 
-    public MainWithoutSort() throws IOException {
-        String line;
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        int nR;
-        int nC;
-        int n;
-        int[] array = null;
+    public static class Median {
+        private static int a, b;
 
-        while (!(line = in.readLine()).equals(CMD_END)) {
-            if (line.startsWith(CMD_RASTER)) {
-                st = new StringTokenizer(line);
-                st.nextToken();
+        private static int[] swap(int[] array, int i, int j) {
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
 
-                nR = Integer.parseInt(st.nextToken());
-                nC = Integer.parseInt(st.nextToken());
-
-                array = new int[nR * nC];
-
-                for (int i = 0; i < nR; i++) {
-                    line = in.readLine();
-                    st = new StringTokenizer(line);
-
-                    for (int j = 0; j < nC; j++) {
-                        array[i * nC + j] = Integer.parseInt(st.nextToken());
-                    }
-                }
-
-                System.out.println(TXT_RASTER);
-            } else if (line.startsWith(CMD_AMPLITUDE)) {
-                System.out.println(getAmplitude(array));
-            } else if (line.startsWith(CMD_PERCENTILE)) {
-                st = new StringTokenizer(line);
-                st.nextToken();
-
-                n = Integer.parseInt(st.nextToken());
-
-                line = in.readLine();
-                st = new StringTokenizer(line);
-
-                for (int i = 0; i < n; i++) {
-                    int value = Integer.parseInt(st.nextToken());
-
-                    System.out.print(getPercentile(array, value));
-
-                    if (i != n - 1) {
-                        System.out.print(" ");
-                    }
-                }
-
-                System.out.print("\n");
-
-            } else if (line.startsWith(CMD_MEDIAN)) {
-                System.out.println(getMedian(array, array.length));
-            }
+            return array;
         }
-    }
 
-    private int[] swap(int[] array, int i, int j) {
-        int tmp = array[i];
-        array[i] = array[j];
-        array[j] = tmp;
-
-        return array;
-    }
-
-    private int getPartition(int[] array, int l, int r) {
-        int lst = array[r];
-        int i = l;
-        int j = l;
-
-        while (j < r) {
-            if (array[j] < lst) {
-                swap(array, i, j);
-
-                i++;
+        private static int part(int[] array, int l, int r) {
+            int lst = array[r], i = l, j = l;
+            while (j < r) {
+                if (array[j] < lst) {
+                    swap(array, i, j);
+                    i++;
+                }
+                j++;
             }
 
-            j++;
+            swap(array, i, r);
+
+            return i;
         }
 
-        swap(array, i, r);
+        private static int randomPartition(int[] array, int l, int r) {
+            int n = r - l + 1;
+            int pivot = (int) (Math.random() % n);
+            swap(array, l + pivot, r);
 
-        return i;
-    }
+            return part(array, l, r);
+        }
 
-    private int getRandomPartition(int[] array, int l, int r) {
-        int n = r - l + 1;
-        int p = (int) (Math.random() % n);
+        private static int median_(int[] array, int l, int r, int k) {
 
-        swap(array, l + p, r);
+            if (l <= r) {
+                int partitionIndex = randomPartition(array, l, r);
 
-        return getPartition(array, l, r);
-    }
-
-    private int getMedianInner(int[] array, int l, int r, int k) {
-        if (l <= r) {
-            int idx = getRandomPartition(array, l, r);
-
-            if (idx == k) {
-                b = array[idx];
-
-                if (a != -1) {
-                    return Integer.MIN_VALUE;
+                if (partitionIndex == k) {
+                    b = array[partitionIndex];
+                    if (a != -1)
+                        return Integer.MIN_VALUE;
+                } else if (partitionIndex == k - 1) {
+                    a = array[partitionIndex];
+                    if (b != -1)
+                        return Integer.MIN_VALUE;
                 }
-            } else if (idx == k - 1) {
-                a = array[idx];
 
-                if (b != -1) {
-                    return Integer.MIN_VALUE;
-                }
+                if (partitionIndex >= k) {
+                    return median_(array, l, partitionIndex - 1, k);
+                } else
+                    return median_(array, partitionIndex + 1, r, k);
             }
 
-            if (idx >= k) {
-                return getMedianInner(array, l, idx - 1, k);
-            } else
-                return getMedianInner(array, idx + 1, r, k);
+            return Integer.MIN_VALUE;
         }
 
-        return Integer.MIN_VALUE;
-    }
+        private static int median(int[] array, int n) {
+            int median;
+            a = -1;
+            b = -1;
 
-    private int getMedian(int[] array, int n) {
-        int median;
-        a = -1;
-        b = -1;
+            if (n % 2 == 1) {
+                median_(array, 0, n - 1, n / 2);
+                median = b;
+            } else {
+                median_(array, 0, n - 1, n / 2);
+                median = (a + b) / 2;
+            }
 
-        if (n % 2 == 1) {
-            getMedianInner(array, 0, n - 1, n / 2);
-            median = b;
-        } else {
-            getMedianInner(array, 0, n - 1, n / 2);
-            median = (a + b) / 2;
+           return median;
         }
-
-        return median;
     }
 
-    private int getAmplitude(int[] array) {
+    private int amplitude(int[] array) {
         int min;
         int max;
 
@@ -171,22 +100,81 @@ public class MainWithoutSort {
         return Math.abs(max - min);
     }
 
-    private int getPercentile(int[] array, int value) {
-        int c;
-        int result;
-        int l;
+    private int percentile(int[] array, int value) {
+        int count;
+        int percentile;
 
-        c = 0;
-        l = array.length;
+        count = 0;
         for (int i : array) {
             if (value > i) {
-                c++;
+                count++;
             }
         }
 
-        result = (c * 100) / l;
+        percentile = (count * 100) / array.length;
 
-        return result;
+        return percentile;
+    }
+
+    public MainWithoutSort() throws IOException {
+        int nRows;
+        int nCols;
+        int nValues;
+        int[] raster;
+        String line;
+        BufferedReader in;
+        StringTokenizer st;
+
+        raster = null;
+        in = new BufferedReader(new InputStreamReader(System.in));
+
+        while (!(line = in.readLine()).equals("TCHAU")) {
+            if (line.startsWith("RASTER")) {
+                st = new StringTokenizer(line);
+                st.nextToken();
+
+                nRows = Integer.parseInt(st.nextToken());
+                nCols = Integer.parseInt(st.nextToken());
+
+                raster = new int[nRows * nCols];
+
+                for (int i = 0; i < nRows; i++) {
+                    line = in.readLine();
+                    st = new StringTokenizer(line);
+
+                    for (int j = 0; j < nCols; j++) {
+                        raster[i * nCols + j] = Integer.parseInt(st.nextToken());
+                    }
+                }
+
+                System.out.println("RASTER GUARDADO");
+            } else if (line.startsWith("AMPLITUDE")) {
+                System.out.println(amplitude(raster));
+            } else if (line.startsWith("PERCENTIL")) {
+                st = new StringTokenizer(line);
+                st.nextToken();
+
+                nValues = Integer.parseInt(st.nextToken());
+
+                line = in.readLine();
+                st = new StringTokenizer(line);
+
+                for (int i = 0; i < nValues; i++) {
+                    int value = Integer.parseInt(st.nextToken());
+
+                    System.out.print(percentile(raster, value));
+
+                    if (i != nValues - 1) {
+                        System.out.print(" ");
+                    }
+                }
+
+                System.out.print("\n");
+
+            } else if (line.startsWith("MEDIANA")) {
+                System.out.println(Median.median(raster, raster.length));
+            }
+        }
     }
 
     public static void main(String[] args) throws IOException {
